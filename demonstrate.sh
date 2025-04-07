74824afc9df4941286e0450e5f5d7b75d337da10
Benedikt Würkner <b.wuerkner@gmail.com>
2025-04-07 10:39:35 +0200

    Adding simple shell script to demonstrate some of the possible targets

---
+++ demonstrate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+entries=(
+    "Run the transitioned final output of project A: "
+    "bazel build projects/projectA/package:final"
+    "Run the transitioned final output of project C: "
+    "bazel build projects/projectC/package:final"
+    "Run the multiplyable service with the 'only_carnivorea' target: "
+    "bazel build product/multiplyable_service:only_carnivorea --platforms=//configs/platforms:ProjectA"
+    "Run the multiplyable service with the 'all_animals' target: "
+    "bazel build product/multiplyable_service:all_animals --platforms=//configs/platforms:ProjectA"
+    "Run the multiplyable service with the 'all_names' target: "
+    "bazel build product/multiplyable_service:all_names --platforms=//configs/platforms:ProjectA")
+
+for ((i=0; i<${#entries[@]}; i+=2)); do
+    echo "${entries[i]}"
+    read -p "$(echo "\$ ${entries[i+1]}")" response
+    if [ "$response" = "" ]; then
+        ${entries[i+1]}
+        echo ""
+    else
+        break
+    fi
+done
